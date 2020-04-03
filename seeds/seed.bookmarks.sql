400e88bd38183ffcfe33cf3fb3f65e214b26f502
Caleb King <caleb.king536@gmail.com>
2020-04-03 14:29:32 -0600

    add seed data to bookmarks table

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,63 @@
+insert into bookmarks
+	(title, url, description, rating)
+	values 
+	(
+		'SQL Tutorial - W3Schools',
+		'https://www.w3schools.com/sql/',
+		'Our SQL tutorial will teach you how to use SQL in: MySQL, SQL Server, MS Access, Oracle, Sybase, Informix, ... Start learning SQL now! ... Learn by examples!',
+		3
+	),
+	(
+		'What is SQL? A Beginners Guide to the SQL Programming',
+		'https://learntocodewith.me/posts/sql-guide/',
+		'When working with databases, a programmer might write commands such as: CREATE DATABASE � to create a database. CREATE TABLE � to create tables. SELECT � to find/extract some data from a database. UPDATE � make adjustments and edit data. DELETE � to delete some data.',
+		4
+	),
+	(
+		'SQL Tutorial for Beginners: Learn SQL in 7 Days - Guru99',
+		'https://www.guru99.com/sql.html',
+		'Learn SQL for beginners in just 5 days! Click here to take our free and easy SQL tutorials, right now. No experience required.',
+		2
+	),
+	(
+		'Intro to SQL: Querying and managing data | Khan Academy',
+		'https://www.khanacademy.org/computing/computer-programming/sql',
+		'Learn how to use SQL to store, query, and manipulate data. SQL is a special-purpose programming language designed for managing data in a relational ...',
+		5
+	),
+	(
+		'SQL Tutorial - An Ultimate Guide for Beginners',
+		'https://www.tutorialrepublic.com/sql-tutorial/',
+		'SQL stands for Structured Query Language.',
+		3
+	),
+	(
+		'SQL Tutorial - W3Schools',
+		'https://www.w3schools.com/sql/',
+		'Our SQL tutorial will teach you how to use SQL in: MySQL, SQL Server, MS Access, Oracle, Sybase, Informix, ... Start learning SQL now! ... Learn by examples!',
+		3
+	),
+	(
+		'What is SQL? A Beginners Guide to the SQL Programming',
+		'https://learntocodewith.me/posts/sql-guide/',
+		'When working with databases, a programmer might write commands such as: CREATE DATABASE � to create a database. CREATE TABLE � to create tables. SELECT � to find/extract some data from a database. UPDATE � make adjustments and edit data. DELETE � to delete some data.',
+		4
+	),
+	(
+		'SQL Tutorial for Beginners: Learn SQL in 7 Days - Guru99',
+		'https://www.guru99.com/sql.html',
+		'Learn SQL for beginners in just 5 days! Click here to take our free and easy SQL tutorials, right now. No experience required.',
+		2
+	),
+	(
+		'Intro to SQL: Querying and managing data | Khan Academy',
+		'https://www.khanacademy.org/computing/computer-programming/sql',
+		'Learn how to use SQL to store, query, and manipulate data. SQL is a special-purpose programming language designed for managing data in a relational ...',
+		5
+	),
+	(
+		'SQL Tutorial - An Ultimate Guide for Beginners',
+		'https://www.tutorialrepublic.com/sql-tutorial/',
+		'SQL stands for Structured Query Language.',
+		3
+	);
